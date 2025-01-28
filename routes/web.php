b8b2b8919386ecfd3dc13959c22e0a98765a4e0d
omartins-zs <gabrielmatheus209@gmail.com>
2025-01-28 20:26:34 -0300

    Entendendo e aprendendo as Rotas

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,42 @@
 <?php
 
+use App\Http\Controllers\MainController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/','home');
+Route::view('/', 'home');
+
+// Explicação da Rota
+// Assinatura base de uma rota
+// Route::verb('uri', callback); - o callback é a ação que vai ser executada quando a rota for acionada
+
+// Rota com função anônima
+Route::get('/rota', function () {
+    return "<h1> Olá laravel </h1>";
+});
+
+Route::get('/user', function () {
+    return "<h1> Aqui esta o Usuario </h1>";
+});
+
+Route::get('/injection', function (Request $request) {
+    var_dump($request);
+});
+
+Route::match(['get', 'post'], '/match', function (Request $request) {
+    return "<h1> Aceita GET E POST </h1>";
+});
+
+Route::any('/any', function (Request $request) {
+    return "<h1> Aceita qualquer HTTP verb </h1>";
+});
+
+Route::get('/index', [MainController::class, 'index']);
+
+Route::get('/about', [MainController::class, 'about']);
+
+Route::redirect('/saltar', '/index');
+
+Route::permanentRedirect('/saltar2', '/index');
+
+Route::view('/view', 'home');
