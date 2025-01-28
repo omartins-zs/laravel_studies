a2d01f244ff42c6562138a6904afc6c5aac8a259
omartins-zs <gabrielmatheus209@gmail.com>
2025-01-28 20:27:41 -0300

    Add myName da Rota view com Nome

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.main_layout')
 @section('content')
 
-<p class="display-6 text-secondary text-center py-5">CONTENT</p>
+{{-- <p class="display-6 text-secondary text-center py-5">CONTENT</p> --}}
+{{-- apresentar myName a partir da route::view --}}
+
+@if(!empty($myName))
+    <h1 class="display-4 text-center text-primary">Hello, {{ $myName }}!</h1>
+@endif
 
 @endsection
