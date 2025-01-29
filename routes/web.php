c650340311908e68398209a2bfaef3647d6e4d9d
omartins-zs <gabrielmatheus209@gmail.com>
2025-01-29 00:12:06 -0300

    Aprendendo Route Parametros com Restrições

--- routes/web.php
+++ routes/web.php
@@ -47,17 +47,31 @@ Route::view('/view2', 'home', ['myName' => 'Gabriel Martins']);
 // ROUTE PARAMETROS
 // ----------------------------------------------
 
-Route::get('/valor/{value}',[MainController::class, 'mostrarValor']);
-Route::get('/valores/{value1}/{value2}',[MainController::class, 'mostrarValores']);
-Route::get('/valores2/{value1}/{value2}',[MainController::class, 'mostrarValores2']);
+Route::get('/valor/{value}', [MainController::class, 'mostrarValor']);
+Route::get('/valores/{value1}/{value2}', [MainController::class, 'mostrarValores']);
+Route::get('/valores2/{value1}/{value2}', [MainController::class, 'mostrarValores2']);
 
-Route::get('/opcional/{value1?}',[MainController::class, 'mostrarValorOpcional']);
+Route::get('/opcional/{value1?}', [MainController::class, 'mostrarValorOpcional']);
 
-Route::get('/opcional2/{value1}/{value2?}',[MainController::class, 'mostrarValorOpcional2']);
+Route::get('/opcional2/{value1}/{value2?}', [MainController::class, 'mostrarValorOpcional2']);
 
-Route::get('/user/{user_id}/post/{post_id}',[MainController::class, 'mostrarPosts']);
+Route::get('/user/{user_id}/post/{post_id}', [MainController::class, 'mostrarPosts']);
 
+// ----------------------------------------------
+// ROUTE PARAMETROS COM RESTRIÇÕES
+// ----------------------------------------------
 
-Route::get('/param2/{nome}/{idade}', function ($nome, $idade) {
-    return "<h1> Parametro: $nome - Idade: $idade </h1>";
-});
+Route::get('/exp1/{value}', function ($value) {
+    return $value;
+})->where('value', '[0-9]+');
+
+Route::get('/exp2/{value}', function ($value) {
+    return $value;
+})->where('value', '[A-Za-z0-9]+');
+
+Route::get('/exp3/{value1}/{value2}', function ($value1, $value2) {
+    return "Value1: $value1, Value2: $value2";
+})->where([
+    'value1' => '[0-9]+',   // Apenas números (um ou mais)
+    'value2' => '[A-Za-z]+' // Apenas letras (maiúsculas ou minúsculas, um ou mais caracteres)
+]);
