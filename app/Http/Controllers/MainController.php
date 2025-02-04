3be134b673e358ed1212db8818f97e0f4674e135
omartins-zs <gabrielmatheus209@gmail.com>
2025-02-04 01:28:38 -0300

    Entendendo a Consolidação Sobre Controllers

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,47 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class MainController extends Controller
 {
-    //add metodo index
-    public function index()
+    public function initMethod(): string
     {
-        echo "Index";
-        // return view('welcome');
+        return "Welcome Hello World";
     }
-
-    public function about()
+    public function viewPage(): View
     {
-        echo "about";
-    }
-
-    public function mostrarValor($valor)
-    {
-        echo "Valor enviado pela rota: " . $valor;
-    }
-
-    public function mostrarValores($valor1, $valor2)
-    {
-        echo "Valores enviado pela rota: " . $valor1 . " e " . $valor2;
-    }
-
-    public function mostrarValores2(Request $request, $valor1, $valor2)
-    {
-        echo "Valores enviado pela rota: " . $valor1 . " e " . $valor2;
-    }
-
-    public function mostrarValorOpcional($valor = null)
-    {
-        echo "Valor opcional : " . $valor;
-    }
-    public function mostrarValorOpcional2($valor1, $valor2 = 100)
-    {
-        echo "Valor opcional :  $valor1 e  $valor2";
-    }
-
-    public function mostrarPosts($user_id, $post_id)
-    {
-        echo "Post do usário ID: $user_id e o post com ID: $post_id";
+        return view('home');
     }
 }
