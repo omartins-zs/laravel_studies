7167de0df0cc7d0b04b323d506a9088f24dcab5c
omartins-zs <gabrielmatheus209@gmail.com>
2025-06-27 17:04:39 -0300

    Criando o Controller User(Resource)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,17 +6,59 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function new()
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
     {
-        echo "New User";
+        //
     }
-    // add fucntion edit
-    public function edit()
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
     {
-        echo "Edit User";
+        //
     }
-    public function delete()
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
     {
-        echo "Delete User";
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }
