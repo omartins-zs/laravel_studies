b297be994b876b2e7be76cfc5b413671489e01f2
omartins-zs <gabrielmatheus209@gmail.com>
2025-06-27 17:05:01 -0300

    Criando a Rota para o Tipo de Controller Resource

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,14 @@
 
 use App\Http\Controllers\MainController;
 use App\Http\Controllers\SingleActionController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/init', [MainController::class, 'initMethod'])->name('init');
 Route::get('/view', [MainController::class, 'viewPage'])->name('view');
 
-// Route para Controller Single Action
+// Route para Controller Single Action | Tipo Invokable
 Route::get('/single', SingleActionController::class)->name('single');
+
+// Route para Controller User | Tipo Resource
+Route::resource('users', UserController::class);
