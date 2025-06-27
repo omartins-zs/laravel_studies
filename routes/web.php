db30b3af8295b0c0840b792ae32a718742ee8148
omartins-zs <gabrielmatheus209@gmail.com>
2025-06-27 17:17:54 -0300

    Criando e Organizando Controllers e Rotas em Subpastas

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,6 @@
 <?php
 
-use App\Http\Controllers\ClientController;
-use App\Http\Controllers\MainController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\SingleActionController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\admin\AdminController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/init', [MainController::class, 'initMethod'])->name('init');
-Route::get('/view', [MainController::class, 'viewPage'])->name('view');
-
-// Route para Controller Single Action | Tipo Invokable
-Route::get('/single', SingleActionController::class)->name('single');
-
-// Route para Controller User | Tipo Resource
-// Route::resource('users', UserController::class);
-
-Route::resources([
-    'clients' => ClientController::class,
-    'products' => ProductController::class,
-    'users' => UserController::class
-]);
+Route::get('/admin', [AdminController::class, 'index'])->name('admin');
