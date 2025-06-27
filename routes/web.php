cbf0f90383b958328594ffd75d67ecaa6e7db004
omartins-zs <gabrielmatheus209@gmail.com>
2025-06-27 16:56:12 -0300

    Criando a Rota para o Single Action Controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use App\Http\Controllers\MainController;
+use App\Http\Controllers\SingleActionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/init', [MainController::class, 'initMethod'])->name('init');
 Route::get('/view', [MainController::class, 'viewPage'])->name('view');
+
+// Route para Controller Single Action
+Route::get('/single', SingleActionController::class)->name('single');
