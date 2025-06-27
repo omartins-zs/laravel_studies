87deea98e3a7b6b6038aa6a21c6c4a9d68fd092b
omartins-zs <gabrielmatheus209@gmail.com>
2025-06-27 17:23:11 -0300

    Entendendo o Controller padrão do Laravel

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,10 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    public function cleanUpperCaseString($string): string
+    {
+        // remover os espaços em branco no início e fim de uma string
+        // converter a string para uppercase (maiúsculas)
+        return strtoupper(trim($string));
+    }
 }
